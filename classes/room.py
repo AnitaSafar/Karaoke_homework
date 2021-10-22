8668dbfafb2d289f799f22e863ae2bafa0bed6bd
Anita Safar <ancsa0416@gmail.com>
2021-10-22 15:57:05 +0100

    checkin and checkout methods done

--- classes/room.py
+++ classes/room.py
@@ -3,3 +3,10 @@ class Room:
     def __init__(self, name, capacity):
         self.name = name
         self.capacity = capacity
+        self.inside = []
+
+    def check_in(self, person):
+        self.inside.append(person)
+
+    def check_out(self, person):
+        self.inside.remove(person)
--- tests/room_test.py
+++ tests/room_test.py
@@ -7,16 +7,27 @@ class TestRoom(unittest.TestCase):
     
     def setUp(self):
         self.room = Room("70s", 50)
-        # self.drink_1 = Drink("beer", 2.00)
-        # self.drink_2 = Drink("wine", 3.00)
-        # self.drink_3 = Drink("gin", 4.00)
-        # self.pub = Pub("The Prancing Pony", 100.00)
-        # self.customer = Customer("Frodo", 10.00)
-    
+        self.guest_1 = Guest("Anna", 34)
+        self.guest_2 = Guest("Lily", 32)
+
     def test_room_has_name(self):
         self.assertEqual("70s", self.room.name)
 
     def test_room_has_capacity(self):
         self.assertEqual(50, self.room.capacity)
 
-    
+    def test_check_in_guest(self):
+        self.room.check_in(self.guest_1)
+        self.assertEqual(1, len(self.room.inside))
+
+    def test_check_out(self):
+        self.room.check_in(self.guest_1)
+        self.room.check_out(self.guest_1)
+        self.assertEqual(0, len(self.room.inside))
+        
+    def test_check_out_1_left(self):
+        self.room.check_in(self.guest_1)        
+        self.room.check_in(self.guest_2)
+        self.room.check_out(self.guest_1)
+        self.assertEqual(1, len(self.room.inside))
+
